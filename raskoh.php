690c40f2aed9d1bf714bf81c6667b7721409eac8
Azi Baloch <azibaloch247@gmail.com>
2015-06-25 14:49:49 +0500

    fixed typos

--- raskoh.php
+++ raskoh.php
@@ -4,7 +4,7 @@
 * Author: Azi Baloch
 * Author URI: http://www.azibaloch.com
 * Description: Easy Post Type and Taxonomy Registration
-* Note: this plugin will not do anything in you admin interface this just includes Raskoh in you wordpress
+* Note: this plugin will not do anything in your admin interface this just includes Raskoh in your wordpress
 * then you can use it in your themes and plugins
 */
 
